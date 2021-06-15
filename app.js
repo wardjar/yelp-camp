e864c5eff68b5174b22f0b2caaaade3b69d22c20
unknown <wardjaradat44@gmail.com>
2021-06-15 17:59:44 +0300

    restart

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
 const MongoDBStore = require("connect-mongo")(session);
-// process.env.DB_URL ||
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
@@ -156,8 +156,7 @@ app.all('*', (req, res, next) => {
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
     if (!err.message) err.message = 'Oh No, Something Went Wrong!'
-    // res.status(statusCode).render('error', { err })
-    console.log(err);
+    res.status(statusCode).render('error', { err })
 })
 
 const port = process.env.PORT || 3000;
